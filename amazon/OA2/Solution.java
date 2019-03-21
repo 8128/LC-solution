920ccc32e698e7680c745d0d2c4b97ef57039e05
tty <tty8128@outlook.com>
2019-03-21 07:49:15 -0400

    Create Solution.java

---
+++ amazon/OA2/Solution.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Solution{
+
+    public int countKDistinctSubstring1(String inputString, int num){
+        int ans=0;
+        for(int i=0;i<inputString.length();i++){
+            HashMap<Character, Integer> hm = new HashMap<>();
+            for(int j=i;j<inputString.length();j++){
+                hm.put(inputString.charAt(j), hm.getOrDefault(inputString.charAt(j),0)+1);
+                if(hm.size()==num){
+                    ans++;
+                    System.out.println(inputString.substring(i, j+1));
+                } 
+                if(hm.size()>num) break;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution mSolution = new Solution();
+        System.out.println(mSolution.countKDistinctSubstring("abasadasfg", 3));
+        System.out.println("----------");;
+        System.out.println(mSolution.countKDistinctSubstring1("abasadasfg", 3));
+    }
+}
