a759509c2d94d15fbe4bb621e821c3edf10b088e
tty <tty8128@outlook.com>
2019-03-22 20:58:01 -0400

    Create 224. Basic Calculator.java

---
+++ amazon revisit/224. Basic Calculator.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    enum number{
+        1,2,3,4,5,6,7,8,9,0
+    }
+    
+    public int calculate(String s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            char temp = s.charAt(i);
+                           
+        }
+    }
+}
