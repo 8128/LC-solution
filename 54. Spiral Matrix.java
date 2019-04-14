adbd3e9ad86333eef2860d9c02dbaa320b09b111
tty <tty8128@outlook.com>
2019-04-14 17:55:26 -0400

    Update 54. Spiral Matrix.java

--- 54. Spiral Matrix.java
+++ 54. Spiral Matrix.java
@@ -1,26 +1,48 @@
+import java.util.*;
+
 class Solution {
+    int[] direc1 = new int[]{0,1,0,-1};
+    int[] direc2 = new int[]{1,0,-1,0};
     public List<Integer> spiralOrder(int[][] matrix) {
-        List ans = new ArrayList();
-        if (matrix.length == 0) return ans;
-        int R = matrix.length, C = matrix[0].length;
-        boolean[][] seen = new boolean[R][C];
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-        int r = 0, c = 0, di = 0;
-        for (int i = 0; i < R * C; i++) {
-            ans.add(matrix[r][c]);
-            seen[r][c] = true;
-            int cr = r + dr[di];
-            int cc = c + dc[di];
-            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]){
-                r = cr;
-                c = cc;
-            } else {
-                di = (di + 1) % 4;
-                r += dr[di];
-                c += dc[di];
-            }
+        List<Integer> ans = new ArrayList<Integer>();
+        int m = matrix.length;
+        if(m==0) return ans;
+        int n = matrix[0].length;
+        //if(m==0 || n==0) return new ArrayList<>();
+        int[][] access = new int[m][n];
+        int direct = 0;
+        int x=0,y=0;
+        for(int i=0;i<m*n;i++){
+            //System.out.println("adding "+x+" "+y);
+            ans.add(matrix[x][y]);
+            access[x][y] = 1;
+            //System.out.println("direction"+direct);
+            if((x+direc1[direct])==-1 || (y+direc2[direct])==-1 || (x+direc1[direct])==m || (y+direc2[direct])==n || access[x+direc1[direct]][y+direc2[direct]]==1){
+                direct = direction(direct);
+            } 
+            x = x+direc1[direct];
+            y = y+direc2[direct];
         }
         return ans;
+    }
+    
+    public int direction(int i){
+        switch(i){
+            case 0: 
+                return 1;
+            case 1: 
+                return 2;
+            case 2: 
+                return 3;
+            case 3:
+                return 0;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution mSolution = new Solution();
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(mSolution.spiralOrder(matrix));
     }
 }
