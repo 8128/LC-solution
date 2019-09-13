ac97fa85ef3d2d56db1fc609f1ad2613d0d545cd
ADKMR <tty8128@outlook.com>
2019-09-13 02:37:21 -0400

    UPDATE

---
+++ citrix/OrderCheck.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class OrderCheck{
+    public int orderCheck(List<Integer> list){
+        List<Integer> temp = new ArrayList(list);
+        Collections.sort(temp);
+        int len = list.size();
+        int ans = 0;
+        for (int i = 0; i < len; i++){
+            if (temp.get(i) != list.get(i)){
+                ans ++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        OrderCheck orderCheck = new OrderCheck();
+        List<Integer> mlist = new LinkedList<>()
+        System.out.println(orderCheck.orderCheck(new int[]{1,1,3,4,1}));
+    }
+}
