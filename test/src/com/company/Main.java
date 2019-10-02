5cec98df08f7e4af49ccf8c695790e40b08e3188
tty <tty8128@outlook.com>
2019-10-01 21:36:06 -0400

    update

--- test/src/com/company/Main.java
+++ test/src/com/company/Main.java
@@ -10,10 +10,21 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        HashMap<String, String> hm = new HashMap<>();
-        System.out.println(hm.size());
-        hm.getOrDefault("aaa","aaa");
-        System.out.println(hm.size());
-
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("abc", 1);
+        hm.put("kfc", 9);
+        hm.put("z", 7);
+        Set<String> set = hm.keySet();
+        List<String> list = new ArrayList<>(set);
+        Set<Map.Entry<String, Integer>> mE = hm.entrySet();
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Integer a = hm.get(o1);
+                Integer b = hm.get(o2);
+                return a.compareTo(b);
+            }
+        });
+        System.out.println(list);
     }
 }
