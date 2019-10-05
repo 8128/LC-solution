69e6c3bc2f87dbb4981e537576ee7cb716c618b8
tty <tty8128@outlook.com>
2019-10-05 00:36:07 -0400

    update

--- test/src/com/company/wealthfront/Dice.java
+++ test/src/com/company/wealthfront/Dice.java
@@ -9,13 +9,10 @@ import java.util.HashSet;
  */
 public class Dice {
 
-    int[] dic1 = new int[]{1,6};
-    int[] dic2 = new int[]{2,5};
-    int[] dic3 = new int[]{3,4};
-    int[] all = new int[]{1,2,3,4,5,6};
+    private int[] all = new int[]{1,2,3,4,5,6};
 
 
-    public int dice (int[] d) {
+    int dice (int[] d) {
         int ans = Integer.MAX_VALUE;
         for (int i : all) {
             ans = Math.min(ans, check(d, i));
@@ -23,27 +20,18 @@ public class Dice {
         return ans;
     }
 
-    public int check (int[] d, int target) {
+    int check (int[] d, int target) {
         int rotate = 0;
         for (int i : d) {
             if (i == target) {
                 continue;
-            } else if ( (has(dic1, target) && has(dic1, i)) || (has(dic2, target) && has(dic2, i)) || (has(dic3, target) && has(dic3, i))) {
+            } else if ( i + target == 7) {
                 rotate += 2;
             } else {
                 rotate++;
             }
         }
         return rotate;
-    }
-
-    public boolean has (int[] d, int target) {
-        for (int i : d) {
-            if (target == i) {
-                return true;
-            }
-        }
-        return false;
     }
 
     public static void main(String[] args) {
