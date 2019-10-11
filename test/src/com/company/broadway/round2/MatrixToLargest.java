6119512bb1044575d27486b131d8cf8e64480b42
tty <tty8128@outlook.com>
2019-10-10 23:24:25 -0400

    update

---
+++ test/src/com/company/broadway/round2/MatrixToLargest.java
@@ -0,0 +1,8 @@
+package com.company.broadway.round2;
+
+/**
+ * @author ：Tianyi Tang
+ * @date ：Created in 2019-10-10 23:12
+ */
+public class MatrixToLargest {
+}
--- test/src/com/company/broadway/round2/TaskScheduler.java
+++ test/src/com/company/broadway/round2/TaskScheduler.java
@@ -1,10 +1,18 @@
 package com.company.broadway.round2;
 
+import java.util.*;
+
 /**
  * @author ：Tianyi Tang
  * @date ：Created in 2019-10-08 14:53
  */
 public class TaskScheduler {
+
+    class Task {
+        String name;
+        List<Task> dependencies;
+
+    }
 
     public void addDependency() {
 
