da1e1a9156de30d8b5860b1c47272d018b3f53d2
tty <tty8128@outlook.com>
2019-10-16 16:21:37 -0400

    UPDATE

---
+++ test/src/com/company/viagogo/ManhattanDistance.java
@@ -0,0 +1,15 @@
+package com.company.viagogo;
+
+import java.util.Scanner;
+
+/**
+ * @author ：Tianyi Tang
+ * @date ：Created in 2019-10-16 15:32
+ */
+public class ManhattanDistance {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ test/src/com/company/viagogo/Tickets.java
@@ -0,0 +1,8 @@
+package com.company.viagogo;
+
+/**
+ * @author ：Tianyi Tang
+ * @date ：Created in 2019-10-16 15:43
+ */
+public class Tickets {
+}
