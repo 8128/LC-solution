aa784d0f069d6daf8ee63c0c39c6f7e2bcae9404
tty <tty8128@outlook.com>
2019-10-27 19:07:18 -0400

    update

--- test/src/com/company/Main.java
+++ test/src/com/company/Main.java
@@ -6,21 +6,8 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        hm.put("abc", 1);
-        hm.put("kfc", 9);
-        hm.put("z", 7);
-        Set<String> set = hm.keySet();
-        List<String> list = new ArrayList<>(set);
-        Set<Map.Entry<String, Integer>> mE = hm.entrySet();
-        Collections.sort(list, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                Integer a = hm.get(o1);
-                Integer b = hm.get(o2);
-                return a.compareTo(b);
-            }
-        });
-        System.out.println(list);
+        int[] temp = new int[]{2,3,1,9,6,43,6};
+        Arrays.sort(temp);
+        System.out.println(Arrays.toString(temp));
     }
 }
