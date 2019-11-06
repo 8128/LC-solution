94776e2c770d4f324a9749ada1184f5a5d113392
tty <tty8128@outlook.com>
2019-11-06 00:48:58 -0500

    update

---
+++ test/src/com/v/blend/voonsite/FractionToDecimal.java
@@ -0,0 +1,47 @@
+package com.v.blend.voonsite;
+
+import java.util.*;
+
+/**
+ * @author ：Tianyi Tang
+ * @date ：Created in 2019-11-06 00:06
+ */
+public class FractionToDecimal {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder fraction = new StringBuilder();
+        // If either one is negative (not both)
+        if (numerator < 0 ^ denominator < 0) {
+            fraction.append("-");
+        }
+        // Convert to Long or else abs(-2147483648) overflows
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+        fraction.append(String.valueOf(dividend / divisor));
+        long remainder = dividend % divisor;
+        if (remainder == 0) {
+            return fraction.toString();
+        }
+        fraction.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                fraction.insert(map.get(remainder), "(");
+                fraction.append(")");
+                break;
+            }
+            map.put(remainder, fraction.length());
+            remainder *= 10;
+            fraction.append(String.valueOf(remainder / divisor));
+            remainder %= divisor;
+        }
+        return fraction.toString();
+    }
+
+    public static void main(String[] args) {
+        FractionToDecimal fractionToDecimal = new FractionToDecimal();
+        System.out.println(fractionToDecimal.fractionToDecimal(2,3));
+    }
+}
