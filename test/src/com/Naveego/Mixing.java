7aeac0bb04ff664cbead0a462ea6e6ec53d81661
tty <tty8128@outlook.com>
2019-11-13 02:11:06 -0500

    update

---
+++ test/src/com/Naveego/Mixing.java
@@ -0,0 +1,80 @@
+package com.Naveego;
+
+/**
+ * @author :   Tianyi Tang
+ * @date :   Created in 2019-11-13 02:08
+ * @leetcode :
+ * @timeComplexity :
+ * @spaceComplexity :
+ */
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Mixing {
+
+    public static String mix(String s1, String s2) {
+
+        return IntStream.range('a', 'z' + 1)
+                .mapToObj(c -> Ch.from((char) c, s1, s2))
+                .filter(Ch::isValid)
+                .sorted()
+                .map(Ch::toString)
+                .collect(Collectors.joining("/"));
+    }
+}
+
+class Ch implements Comparable<Ch> {
+
+    private final int source;
+    private final char c;
+    private final long count;
+    private final String stringRep;
+
+    private Ch(int source, char c, long count) {
+
+        this.source = source;
+        this.c = c;
+        this.count = count;
+        stringRep = buildStringRep();
+    }
+
+    static Ch from(char c, String s1, String s2) {
+
+        long count1 = s1.chars().filter(ch -> ch == c).count();
+        long count2 = s2.chars().filter(ch -> ch == c).count();
+        return count1 > count2 ? new Ch(1, c, count1)
+                : count1 == count2 ? new Ch(0, c, count1)
+                : new Ch(2, c, count2);
+    }
+
+    boolean isValid() {
+
+        return count > 1;
+    }
+
+    private String buildStringRep() {
+
+        String src = source == 0 ? "=" : String.valueOf(source);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(c);
+        }
+        return String.format("%s:%s", src, sb);
+    }
+
+    @Override
+    public int compareTo(Ch o) {
+
+        int byCount = Long.compare(o.count, count);
+        if (byCount != 0) {
+            return byCount;
+        }
+        return stringRep.compareTo(o.stringRep);
+    }
+
+    @Override
+    public String toString() {
+
+        return stringRep;
+    }
+}
--- test/src/com/v/Main.java
+++ test/src/com/v/Main.java
@@ -4,8 +4,170 @@ import java.util.*;
 
 public class Main {
 
+    public static String mix(String s1, String s2) {
+        // your code
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        HashMap<Character, StringBuilder> hm1 = new HashMap<>();
+        HashMap<Character, StringBuilder> hm2 = new HashMap<>();
+        HashSet<Character> seen = new HashSet<>();
+        for (char c : c1) {
+            if(c - 'a' >= 26 || c - 'a' < 0){
+                continue;
+            }
+            StringBuilder sb = hm1.getOrDefault(c, new StringBuilder());
+            sb.append(c);
+            hm1.put(c, sb);
+        }
+        for (char c : c2) {
+            if(c - 'a' >= 26 || c - 'a' < 0){
+                continue;
+            }
+            StringBuilder sb = hm2.getOrDefault(c, new StringBuilder());
+            sb.append(c);
+            hm2.put(c, sb);
+        }
+        List<Character> key1 = new ArrayList<>(hm1.keySet());
+        List<Character> key2 = new ArrayList<>(hm2.keySet());
+        for (Character c : key1) {
+            if (hm1.get(c).length() <= 1) {
+                hm1.remove(c);
+            }else if (hm2.containsKey(c) && hm2.get(c).length() > hm1.get(c).length()) {
+                hm1.remove(c);
+            }
+        }
+        for (Character c : key2) {
+            if (hm2.get(c).length() <= 1) {
+                hm2.remove(c);
+            }else if (hm1.containsKey(c) && hm1.get(c).length() >= hm2.get(c).length()) {
+                hm2.remove(c);
+            }
+        }
+        key1 = new ArrayList<>(hm1.keySet());
+        key2 = new ArrayList<>(hm2.keySet());
+        key1.sort(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                if (hm1.get(o2).length() == hm1.get(o1).length()){
+                    return Integer.compare(o1, o2);
+                }
+                return hm1.get(o2).length() - hm1.get(o1).length();
+            }
+        });
+        key2.sort(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                if (hm2.get(o2).length() == hm2.get(o1).length()){
+                    return Integer.compare(o1, o2);
+                }
+                return hm2.get(o2).length() - hm2.get(o1).length();
+            }
+        });
+        StringBuilder ans = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < key1.size() && j < key2.size()) {
+            if (seen.contains(key1.get(i))) {
+                i++;
+                continue;
+            }
+            if (seen.contains(key2.get(j))) {
+                j++;
+                continue;
+            }
+            if (hm1.get(key1.get(i)).length() > hm2.get(key2.get(j)).length()){
+                seen.add(key1.get(i));
+                ans.append("1:").append(hm1.get(key1.get(i))).append("/");
+                i++;
+                while(i < s1.length() && hm1.get(key1.get(i)).length() == hm1.get(key1.get(i-1)).length()){
+                    if (seen.contains(key1.get(i))) {
+                        i++;
+                        continue;
+                    }
+                    ans.append("1:").append(hm1.get(key1.get(i))).append("/");
+                    seen.add(key1.get(i));
+                    i++;
+                }
+            }else if (hm1.get(key1.get(i)).length() < hm2.get(key2.get(j)).length()){
+                seen.add(key2.get(j));
+                ans.append("2:").append(hm2.get(key2.get(j))).append("/");
+                j++;
+                while(j < s2.length() && hm2.get(key2.get(j)).length() == hm2.get(key2.get(j-1)).length()){
+                    if (seen.contains(key2.get(j))) {
+                        j++;
+                        continue;
+                    }
+                    ans.append("2:").append(hm2.get(key2.get(j))).append("/");
+                    seen.add(key2.get(j));
+                    j++;
+                }
+            } else {
+                if (key1.get(i) <= key2.get(j)) {
+                    seen.add(key1.get(i));
+                    ans.append("1:").append(hm1.get(key1.get(i))).append("/");
+                    i++;
+                    while(i < key1.size() && hm1.get(key1.get(i)).length() == hm1.get(key1.get(i-1)).length()){
+                        if (seen.contains(key1.get(i))) {
+                            i++;
+                            continue;
+                        }
+                        ans.append("1:").append(hm1.get(key1.get(i))).append("/");
+                        seen.add(key1.get(i));
+                        i++;
+                    }
+                    ans.append("=:").append(hm2.get(key2.get(j))).append("/");
+                    j++;
+                    while(j < key2.size() && hm2.get(key2.get(j)).length() == hm2.get(key2.get(j-1)).length()){
+                        if (seen.contains(key2.get(j))) {
+                            j++;
+                            continue;
+                        }
+                        ans.append("=:").append(hm2.get(key2.get(j))).append("/");
+                        seen.add(key2.get(j));
+                        j++;
+                    }
+                }else{
+                    ans.append("2:").append(hm2.get(key2.get(j))).append("/");
+                    seen.add(key2.get(j));
+                    j++;
+                    while(j < key2.size() && hm2.get(key2.get(j)).length() == hm2.get(key2.get(j-1)).length()){
+                        if (seen.contains(key2.get(j))) {
+                            j++;
+                            continue;
+                        }
+                        ans.append("2:").append(hm2.get(key2.get(j))).append("/");
+                        seen.add(key2.get(j));
+                        j++;
+                    }
+                    ans.append("=:").append(hm1.get(key1.get(i))).append("/");
+                    i++;
+                    while(i < key1.size() && hm1.get(key1.get(i)).length() == hm1.get(key1.get(i-1)).length()){
+                        if (seen.contains(key1.get(i))) {
+                            i++;
+                            continue;
+                        }
+                        ans.append("=:").append(hm1.get(key1.get(i))).append("/");
+                        seen.add(key1.get(i));
+                        i++;
+                    }
+                }
+            }
+        }
+        if (i < key1.size()) {
+            ans.append("1:").append(hm1.get(key1.get(i))).append("/");
+            i++;
+        }
+        if (j < key2.size()) {
+            ans.append("2:").append(hm2.get(key2.get(j))).append("/");
+            j++;
+        }
+        if(ans.length()>0) {
+            ans.deleteCharAt(ans.length()-1);
+        }
+        return ans.toString();
+    }
 
 
     public static void main(String[] args) {
+        System.out.println(mix("Are they here", "yes, they are here"));
     }
 }
