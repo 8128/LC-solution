ca4abf93cce653e8ede9d73ac21421844761450e
tty <tty8128@outlook.com>
2019-12-09 00:13:14 -0500

    update

--- test/src/com/Contest.java
+++ test/src/com/Contest.java
@@ -10,48 +10,72 @@ import java.util.*;
  * @spaceComplexity :
  */
 class Contest {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        int len = searchWord.length();
-        char[] sead = searchWord.toCharArray();
-        HashMap<Integer, List<String>> hm = new HashMap<>();
-        for (String str : products) {
-            int count = 0;
-            char[] cs = str.toCharArray();
-            for (int i = 0; i < len && i < cs.length; i++) {
-                if (cs[i] == sead[i]) {
-                    count++;
-                }else {
-                    break;
+    public int minFlips(int[][] mat) {
+        HashSet<String> hs = new HashSet<>();
+        int[][] dest = new int[mat.length][mat[0].length];
+        String des = Arrays.deepToString(dest);
+        hs.add(Arrays.deepToString(mat));
+        Queue<int[][]> q = new LinkedList<>();
+        q.add(mat);
+        if (hs.contains(des)) {
+            return 0;
+        }
+        int level = 0;
+        while (!q.isEmpty()) {
+            level++;
+            if (level > 9) {
+                return -1;
+            }
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                int[][] queout = q.poll();
+                for (int m = 0; m < mat.length; m++) {
+                    for (int n = 0; n < mat[0].length; n++) {
+                        int[][] temp = deepcopy(queout);
+                        if (m + 1 < mat.length) {
+                            temp[m+1][n] = temp[m+1][n] == 1 ? 0 : 1;
+                        }
+                        if (m - 1 >= 0) {
+                            temp[m-1][n] = temp[m-1][n] == 1 ? 0 : 1;
+                        }
+                        if (n + 1 < mat[0].length) {
+                            temp[m][n + 1] = temp[m][n + 1] == 1 ? 0 : 1;
+                        }
+                        if (n - 1 >= 0) {
+                            temp[m][n - 1] = temp[m][n - 1] == 1 ? 0 : 1;
+                        }
+                        temp[m][n] = temp[m][n] == 1 ? 0 : 1;
+                        String str = Arrays.deepToString(temp);
+                        if (hs.contains(str)) {
+                            continue;
+                        }
+                        q.offer(temp);
+                        hs.add(str);
+                        if (hs.contains(des)) {
+                            return level;
+                        }
+                    }
                 }
             }
-            List<String> temp = hm.getOrDefault(count, new ArrayList<>());
-            temp.add(str);
-            hm.put(count, temp);
         }
-        LinkedList<List<String>> ans = new LinkedList<>();
-        for (int i = 1; i <= len; i++) {
-            LinkedList<String> temp = new LinkedList<>();
-            int k = i;
-            while (k <= len) {
-                temp.addAll(hm.getOrDefault(k, new ArrayList<>()));
-                k++;
-            }
-            Collections.sort(temp);
-            if (temp.size()>3) {
-                List<String> list = temp.subList(0,3);
-                ans.addLast(list);
-            } else {
-                ans.addLast(temp);
-            }
+        return -1;
+    }
+
+    public int[][] deepcopy (int[][] input) {
+        int[][] ans = new int[input.length][input[0].length];
+        int i = 0;
+        for (int[] temp : input) {
+            ans[i] = temp.clone();
+            i++;
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        String[] products = new String[]{"havana"};
-        String search = "tatiana";
         Contest contest = new Contest();
-        System.out.println(contest.suggestedProducts(products, search));
+        System.out.println(contest.minFlips(new int[][]{{0,0},{0,1}}));
     }
+
+
 
 }
