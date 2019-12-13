ac244caac85a0923e4faccb63d205ad6f9268481
tty <tty8128@outlook.com>
2019-12-13 15:50:50 -0500

    Update OddEvenOperations.java

--- test/src/com/v/mapbox/OddEvenOperations.java
+++ test/src/com/v/mapbox/OddEvenOperations.java
@@ -9,39 +9,7 @@ package com.v.mapbox;
  */
 public class OddEvenOperations {
     public static int oddEvenOperations(int[] in){
-        int l = 0;
-        int r = in.length - 1;
-        int sum = 0;
-        int ans = 0;
-        int[] dp = new int[in.length + 1];
-        for (int i : in) {
-            sum += i;
-        }
-        dp[0] = sum;
-        for (int i = 1; i <= in.length; i++) {
-            if (i % 2 == 0) {
-                if (in[l] > in[r]) {
-                    ans = ans - (sum - in[l]);
-                    l++;
-                    sum -= in[l];
-                } else {
-                    ans = ans - (sum - in[r]);
-                    r--;
-                    sum -= in[r];
-                }
-            } else {
-                if (in[l] > in[r]) {
-                    ans = ans + (sum - in[r]);
-                    r--;
-                    sum -= in[r];
-                } else {
-                    ans = ans + (sum - in[l]);
-                    l++;
-                    sum -= in[l];
-                }
-            }
-        }
-        return ans;
+        
     }
 
     public static void main(String[] args) {
